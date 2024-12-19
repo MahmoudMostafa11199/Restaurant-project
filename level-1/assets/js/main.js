18ef5980a188dbd57dce83e10f3cb9c930f85f86
nayera540 <1900205@eng.asu.edu.eg>
2024-12-19 18:02:44 +0200

    adding home section 4 and 6

---
+++ level-1/assets/js/main.js
@@ -0,0 +1,7 @@
+function playVideo() {
+    const thumbnail = document.getElementById('thumbnail');
+    const videoContainer = document.getElementById('video-container');
+
+    thumbnail.style.display = 'none';
+    videoContainer.style.display = 'block';
+}
