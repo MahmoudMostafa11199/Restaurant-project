a7c7f57058f19189f45a17ab700992bcec8d474a
Mahmoud Mostafa <m_devolepment@hotmail.com>
2024-12-19 18:13:10 +0200

    Edit level-1

---
+++ level-1/assets/js/main.js
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////
+// Contact
+const animatedElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    root: null,
+    threshold: 0.1,
+  }
+);
+
+animatedElements.forEach((element) => observer.observe(element));
+
+//////////////////////////////////////////////////
+// Swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  loop: true,
+  centredSlides: true,
+  speed: 5000,
+  allowTouchMove: true,
+  autoplay: {
+    delay: 0,
+    disabledOnInteraction: false,
+  },
+});
